200c669a536d1e68ef3241311172ea74fd967163
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-11-30 18:04:32 +0900

    init

---
+++ resources/js/app.tsx
@@ -0,0 +1,18 @@
+import "./bootstrap";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import BasicExample from "./components/BasicExample";
+
+function App() {
+    return (
+        <div style={{ padding: "40px" }}>
+            <h1 style={{ color: "gold" }}>React App</h1>
+            <div>
+                <BasicExample />
+            </div>
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
+root.render(<App />);
---
+++ resources/js/components/BasicExample.tsx
@@ -0,0 +1,26 @@
+import Alert from "react-bootstrap/Alert";
+
+function BasicExample() {
+    return (
+        <>
+            <div>
+                {[
+                    "primary",
+                    "secondary",
+                    "success",
+                    "danger",
+                    "warning",
+                    "info",
+                    "light",
+                    "dark",
+                ].map((variant) => (
+                    <Alert key={variant} variant={variant}>
+                        This is a {variant} alert—check it out!!
+                    </Alert>
+                ))}
+            </div>
+        </>
+    );
+}
+
+export default BasicExample;
