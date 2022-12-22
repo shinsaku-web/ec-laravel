060e009b36abb6f5e6ddfc3f33829ebe22dc3ec2
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-12-22 19:57:54 +0900

    ownerとadminのログインページを作成

--- resources/js/pages/admin/login.tsx
+++ resources/js/pages/admin/login.tsx
@@ -1,5 +1,5 @@
-import { GuestPageLayout } from "../components/organisms/GuestPageLayout";
-import { LoginForm } from "../components/organisms/LoginForm";
+import { GuestPageLayout } from "../../components/organisms/GuestPageLayout";
+import { LoginForm } from "../../components/organisms/LoginForm";
 
 export const LoginUserPage = () => {
     return (
@@ -7,7 +7,7 @@ export const LoginUserPage = () => {
             <GuestPageLayout>
                 <div style={{ maxWidth: "600px", margin: "auto" }}>
                     <h3 className="pb-4 pt-4 text-center">ログイン</h3>
-                    <LoginForm userType="user" />
+                    <LoginForm userType="admin" />
                 </div>
             </GuestPageLayout>
         </div>
--- resources/js/pages/owner/login.tsx
+++ resources/js/pages/owner/login.tsx
@@ -1,7 +1,15 @@
+import { GuestPageLayout } from "../../components/organisms/GuestPageLayout";
+import { LoginForm } from "../../components/organisms/LoginForm";
+
 export const LoginOwnerPage = () => {
     return (
         <div>
-            <p>オーナーログインページ</p>
+            <GuestPageLayout>
+                <div style={{ maxWidth: "600px", margin: "auto" }}>
+                    <h3 className="pb-4 pt-4 text-center">ログイン</h3>
+                    <LoginForm userType="owner" />
+                </div>
+            </GuestPageLayout>
         </div>
     );
 };
