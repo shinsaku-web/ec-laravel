f408c611ca646e140079e483da4b8b1bdb396d1f
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-12-29 12:47:32 +0900

    shop変更

--- resources/js/pages/owner/shops.tsx
+++ resources/js/pages/owner/shops.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import { ApiClient } from "../../apis/ApiClient";
+import { Shop } from "../../types/shop";
+
 export const ShopsPage = () => {
+    const [shops, setShops] = useState<Shop[]>([]);
+    useEffect(() => {
+        (async () => {
+            try {
+                const { data } = await ApiClient("/api/owner/shop");
+                setShops(data);
+            } catch (error) {
+                console.error(error);
+            }
+        })();
+    }, []);
     return (
         <div>
             <p>店舗一覧ページ</p>
+            <p>
+                {shops.map((shop) => (
+                    <div key={shop.id}>
+                        <div>{shop.name}</div>
+                    </div>
+                ))}
+            </p>
         </div>
     );
 };
