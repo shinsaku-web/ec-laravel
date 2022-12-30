e50a49e689de3dd875a5553c8670d9d8cf7f9bcb
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-12-30 17:36:48 +0900

    データ表示

--- resources/js/pages/owner/shops/edit.tsx
+++ resources/js/pages/owner/shops/edit.tsx
@@ -1,15 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { ApiClient } from "../../../apis/ApiClient";
+import { Shop } from "../../../types/shop";
 
 export const ShopEdit = () => {
     const { id } = useParams();
     const navigate = useNavigate();
+    const [shop, setShop] = useState<Shop | null>(null);
     useEffect(() => {
         (async () => {
             try {
                 const { data } = await ApiClient("/api/owner/shop/" + id);
-                console.log(data);
+                setShop((prev) => ({ ...prev, ...data }));
             } catch (error) {
                 console.error(error);
                 navigate("/404");
@@ -17,10 +19,16 @@ export const ShopEdit = () => {
         })();
     }, []);
 
+    if (!shop) {
+        return <div>データの取得に失敗しました。</div>;
+    }
+
     return (
         <div>
             <h2>edit shop</h2>
             <div>test</div>
+            <p>{shop.name}</p>
+            <p>{shop.information}</p>
         </div>
     );
 };
