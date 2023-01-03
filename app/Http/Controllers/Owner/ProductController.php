59b3b0665c446e8358cbd3ec06244163645df19d
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2023-01-03 12:17:51 +0900

    コントローラーをowner配下に

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Owner;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
 use App\Models\Product;
