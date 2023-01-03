0d687c99d06dca148019503e69384f03109ccde6
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2023-01-03 12:58:11 +0900

    商品のダミーデータ投入

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -18,13 +17,47 @@ class ProductSeeder extends Seeder
         DB::table("products")->insert([
             [
                 "shop_id" => 1,
+                "name" => "商品名が入ります",
+                "information" => "商品情報が入ります。",
+                "price" => 10000,
+                "is_selling" => 1,
+                "sort_order" => 1,
                 "secondary_category_id" => 1,
-                "image1" => 1
+                "image1" => 1,
+                "image2" => 2,
             ],
             [
                 "shop_id" => 1,
+                "name" => "商品名が入ります",
+                "information" => "商品情報が入ります。",
+                "price" => 20000,
+                "is_selling" => 0,
+                "sort_order" => 2,
                 "secondary_category_id" => 2,
-                "image1" => 2
+                "image1" => 5,
+                "image2" => 6,
+            ],
+            [
+                "shop_id" => 2,
+                "name" => "商品名が入ります",
+                "information" => "商品情報が入ります。",
+                "price" => 30000,
+                "is_selling" => 1,
+                "sort_order" => 1,
+                "secondary_category_id" => 3,
+                "image1" => 2,
+                "image2" => 4,
+            ],
+            [
+                "shop_id" => 2,
+                "name" => "商品名が入ります",
+                "information" => "商品情報が入ります。",
+                "price" => 40000,
+                "is_selling" => 0,
+                "sort_order" => 2,
+                "secondary_category_id" => 4,
+                "image1" => 1,
+                "image2" => 3,
             ],
         ]);
     }
