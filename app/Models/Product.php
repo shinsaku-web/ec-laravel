e9af9cb2921bf7539be99f672198f41427ba78d7
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2023-01-08 08:39:23 +0900

    リレーションミス修正

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -25,7 +25,7 @@ class Product extends Model
 
     public function shop()
     {
-        return $this->belongsTo(Owner::class);
+        return $this->belongsTo(Shop::class);
     }
 
     public function category()
